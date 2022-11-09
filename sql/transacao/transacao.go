2f9610751650f6e4c1a6208f9f1269212c371446
unknown <nantorres0@gmail.com>
2022-11-09 10:29:51 -0300

    Curso Go(Golang)

---
+++ sql/transacao/transacao.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "root:123456@/cursogo")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	tx, _ := db.Begin()
+	stmt, _ := tx.Prepare("insert into usuarios(id, nome) values(?, ?)")
+
+	stmt.Exec(2000, "Bia")
+	stmt.Exec(2001, "Carlos")
+	_, err = stmt.Exec(1, "Tiago") // chave duplicada
+
+	if err != nil {
+		tx.Rollback()
+		log.Fatal(err)
+	}
+
+	tx.Commit()
+}
